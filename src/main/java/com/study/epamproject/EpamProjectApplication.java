eab7060465812437455ce6cd7f357d5317597480
Ferox <gipnous@gmail.com>
2019-10-04 23:43:30 +0300

    src commit

---
+++ src/main/java/com/study/epamproject/EpamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.study.epamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpamProjectApplication.class, args);
+	}
+
+}
